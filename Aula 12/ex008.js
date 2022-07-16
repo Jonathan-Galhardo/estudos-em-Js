3637f554e857a3be5fb56f6081c3d591904762b6
Jonathan-Galhardo <galhardoj@gmail.com>
2022-07-16 17:14:23 -0300

    estudos em Js

---
+++ Aula 12/ex008.js
@@ -0,0 +1,19 @@
+var idade = 65
+console.log('Você tem ' + idade + ' anos:')
+if (idade < 16) {
+    console.log('não pode votar')
+    } else{
+        if (idade < 18 || idade >= 65){
+            console.log('voto facultativo')
+        } else{
+            if (idade <65){
+            console.log('voto obrigatório')
+            } 
+
+            }
+            
+            
+    }
+        
+
+    
