f3835111813724d68e087193c255c8f34a93711f
Jonathan-Galhardo <galhardoj@gmail.com>
2022-07-18 11:09:08 -0300

    laco de repetição em Js

--- Exercicios/ex016/script.js
+++ Exercicios/ex016/script.js
@@ -1,27 +1,44 @@
 function contar() {
-    let ini = document.getElementById('txt1')
+    let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
 
     if(ini.value.length == 0 || fim.value.length ==0 || passo.value.length == 0){
-        window.alert('ERRO')
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('ERRO')
+        
     } else {
-        res.innerHTML = 'Contando:'
+        res.innerHTML = 'Contagem: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
-        let p = Number(paso.value)
-
-        for(let c = i; c <= f; c = c + p){
-            res.innerHTML = c
+        let p = Number(passo.value)
+        if (p <=0){
+            window.alert('Passo inválido, considerando passo = 1')
+            p = 1
         }
+        if ( i < f){
+            // contagem crescente
+            for(let c = i; c <= f; c += p){
+            res.innerHTML += `${c} \u{1F680} `
+        }
+        
+        } else {
+            // contagem decrescente
+            for (let c = i; c >= f; c -= p ){
+                res.innerHTML += `${c} \u{1F787}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+        
     }
-
 }
+      
+       
+        
 
-
-/*
-for(var n1 = 1; n1 <= n2; n1 + n2){
-    resultado.innerHTML = nc
-}*/
+        //for(let c = i; c <= f; c = c + p){
+        //    res.innerHTML = {$}c
+        //}
+  //  }
 
